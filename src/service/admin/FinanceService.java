e5f808f04a8ed078261cc4113bee977ff5d9c98a
LiYanBin123456 <1249764963@qq.com>
2020-07-20 16:00:03 +0800

    根据需求变动修改

---
+++ src/service/admin/FinanceService.java
@@ -0,0 +1,25 @@
+package service.admin;
+
+import bean.admin.Finance;
+import dao.admin.FinanceDao;
+import database.DaoQueryResult;
+import database.DaoUpdateResult;
+
+import java.sql.Connection;
+
+public class FinanceService {
+    private FinanceDao financeDao = new FinanceDao();
+
+
+    public DaoUpdateResult updateFinance(Connection conn, Finance finance) {
+        return financeDao.updateFinance(conn,finance);
+    }
+
+    public DaoQueryResult getFinance(Connection conn, long cid) {
+        return financeDao.getFinance(conn,cid);
+    }
+
+    public DaoUpdateResult insertFinance(Connection conn, Finance finance) {
+        return financeDao.insertFinance(conn,finance);
+    }
+}
