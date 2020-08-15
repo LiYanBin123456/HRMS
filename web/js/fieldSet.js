f3f46603c22fcd4a7f8d2e3b9d3435f241dd0d90
xinKing-1 <15293077193@163.com>
2020-08-15 18:13:56 +0800

    修改

--- web/js/fieldSet.js
+++ web/js/fieldSet.js
@@ -78,7 +78,7 @@ var columns_cooperation = [[
 //供应商管理字段集合
 var columns_supplier = [[
     {field:'name', title: '名称'},
-    {field:'business', title: '业务类别',width:90},
+    {field:'business', title: '业务类别',width:90,templet:function (d) { return array_value2text(business_supplier,d.business) }},
     {field:'contact', title: '联系人',width:100},
     {field:'phone', title: '联系电话',width:150},
     {fixed: 'right', title: '操作', toolbar: '#bar_supplier',width:200}
@@ -115,7 +115,7 @@ var columns_contract_employee  = [[
 var columns_employee_internal = [[
     {field:'cardId', title: '身份证号',width:170},
     {field:'name', title: '姓名',width:80},
-    {field:'degree', title: '学历',width:80},
+    {field:'degree', title: '学历',width:80,templet:function (d) { return array_value2text(degrees_employee,d.degree) }},
     {field:'phone', title: '联系电话',width:120},
     {field:'department', title: '所属部门'},
     {title: '操作', toolbar: '#bar_employee',width:360,fixed:"right"}
@@ -126,7 +126,7 @@ var columns_employee_expatriate = [[
     {type:'checkbox'},
     {field:'cardId', title: '身份证号',width:170},
     {field:'name', title: '姓名',width:80},
-    {field:'degree', title: '学历',width:100},
+    {field:'degree', title: '学历',width:100,templet:function (d) { return array_value2text(degrees_employee,d.degree) }},
     {field:'phone', title: '联系电话',width:120},
     {field:'department', title: '派遣单位'},
     { title: '操作', toolbar: '#bar_employee',width:400,fixed:"right"}
@@ -136,7 +136,7 @@ var columns_employee_expatriate = [[
 var columns_employee_spare = [[
     {field:'cardId', title: '身份证号',width:200},
     {field:'name', title: '姓名',width:200},
-    {field:'degree', title: '学历',width:230},
+    {field:'degree', title: '学历',width:230,templet:function (d) { return array_value2text(degrees_employee,d.degree) }},
     {field:'phone', title: '联系电话'},
     {field: 'right', title: '操作', toolbar: '#bar_spare',width:200,fixed:"right"}
 ]];
@@ -152,7 +152,7 @@ var columns_settlement1  = [[
     {field:'manage', title: '管理费',width:70},
     {field:'tax', title: '税费',width:70},
     {field:'summary', title: '总额',width:80},
-    {field:'status', title: '状态',width:70},
+    {field:'status', title: '状态',width:70,templet:function (d) { return array_value2text(status_settlement,d.status) }},
     {title:'操作', toolbar: '#bar_settlement',width:260,fixed:"right"}
 ]];
 
@@ -166,7 +166,7 @@ var columns_settlement10  = [[
     {field:'manage', title: '管理费',width:70},
     {field:'tax', title: '税费',width:70},
     {field:'summary', title: '总额',width:80},
-    {field:'status', title: '状态',width:70},
+    {field:'status', title: '状态',width:70,templet:function (d) { return array_value2text(status_settlement,d.status) }},
     {title: '操作', toolbar: '#bar_settlement',width:260,fixed:"right"}
 ]];
 
@@ -180,7 +180,7 @@ var columns_settlement2  = [[
     {field:'traffic', title: '交通费',width:70},
     {field:'extra', title: '附加',width:70},
     {field:'summary', title: '总额',width:80},
-    {field:'status', title: '状态',width:70},
+    {field:'status', title: '状态',width:70,templet:function (d) { return array_value2text(status_settlement,d.status) }},
     {title: '操作', toolbar: '#bar_settlement',width:260,fixed:"right"}
 ]];
 
@@ -193,7 +193,7 @@ var columns_settlement20  = [[
     {field:'traffic', title: '交通费',width:70},
     {field:'extra', title: '附加',width:70},
     {field:'summary', title: '总额',width:80},
-    {field:'status', title: '状态',width:70},
+    {field:'status', title: '状态',width:70,templet:function (d) { return array_value2text(status_settlement,d.status) }},
     {title: '操作', toolbar: '#bar_settlement',width:260,fixed:"right"}
 ]];
 
@@ -204,7 +204,7 @@ var columns_settlement3  = [[
     {field:'month', title: '月份',width:100},
     {field:'pname', title: '保险产品',width:180},
     {field:'price', title: '保费',width:100},
-    {field:'status', title: '状态',width:100},
+    {field:'status', title: '状态',width:100,templet:function (d) { return array_value2text(status_settlement,d.status) }},
     {title: '操作', toolbar: '#bar_settlement',width:260,fixed:"right"}
 ]];
 
@@ -214,7 +214,7 @@ var columns_settlement30  = [[
     {field:'month', title: '月份',width:100,fixed:"left"},
     {field:'pname', title: '保险产品'},
     {field:'price', title: '保费',width:100},
-    {field:'status', title: '状态',width:100},
+    {field:'status', title: '状态',width:100,templet:function (d) { return array_value2text(status_settlement,d.status) }},
     {title: '操作', toolbar: '#bar_settlement',width:260,fixed:"right"}
 ]];
 
@@ -353,7 +353,7 @@ var columns_insured1  = [[
     {field:'start', title: '参保时间',width:80},
     {field:'money', title: '月缴费工资',width:100},
     {field:'entry', title: '工作时间',width:100},
-    {field:'status', title: '参保状态',width:80},
+    {field:'status', title: '参保状态',width:80,templet:function (d) { return array_value2text(status_insurance,d.status) }},
     {fixed: 'right', title: '操作', toolbar: '#bar_insured'}
 ]];
 
@@ -363,7 +363,7 @@ var columns_insured2  = [[
     {field:'name', title: '姓名',width:80},
     {field:'start', title: '起缴时间',width:80},
     {field:'money', title: '工资基数',width:100},
-    {field:'status', title: '参保状态',width:80},
+    {field:'status', title: '参保状态',width:80,templet:function (d) { return array_value2text(status_insurance,d.status) }},
     {fixed: 'right', title: '操作', toolbar: '#bar_insured'}
 ]];
 
@@ -380,8 +380,8 @@ var columns_product = [[
     {field:'fin1', title: '产品保额',width:80},
     {field:'fin2', title: '医疗保额',width:80},
     {field:'allowance', title: '住院津贴',width:80},
-    {field:'period', title: '保障时段',width:100},
-    {field:'allow', title: '可参保人员',width:130},
+    {field:'period', title: '保障时段',width:100,templet:function (d) { return array_value2text(period_product,d.status) }},
+    {field:'allow', title: '可参保人员',width:130,templet:function (d) { return array_value2text(allow_product,d.status) }},
     {field:'min', title: '年龄下限',width:100},
     {field:'max', title: '年龄上限',width:100},
     {fixed: 'right', title: '操作', toolbar: '#bar_product'}
@@ -420,12 +420,53 @@ var categorys_cooperation = [
     {value:3,text:"学校"},
     {value:4,text:"内资企业"},
     {value:5,text:"外资企业"},
-    {value:5,text:"港澳台企业"},
-    {value:5,text:"内资工厂"},
-    {value:5,text:"外资工厂"},
-    {value:5,text:"港澳台工厂"},
-    {value:5,text:"其它"}
+    {value:6,text:"港澳台企业"},
+    {value:7,text:"内资工厂"},
+    {value:8,text:"外资工厂"},
+    {value:9,text:"港澳台工厂"},
+    {value:10,text:"其它"}
 ];
+var business_supplier = [
+    {value:0,text:"顶岗实习"},
+    {value:1,text:"就业安置"},
+    {value:2,text:"其他"}
+];
+var degrees_employee = [
+    {value:0,text:"高中以下"},
+    {value:1,text:"高中"},
+    {value:2,text:"中专"},
+    {value:3,text:"大专"},
+    {value:4,text:"本科"},
+    {value:5,text:"硕士"},
+    {value:6,text:"博士"}
+];
+var status_settlement = [
+    {value:0,text:"编辑"},
+    {value:1,text:"提交"},
+    {value:2,text:"一审"},
+    {value:3,text:"二审"},
+    {value:4,text:"终审"},
+    {value:5,text:"扣款"},
+    {value:6,text:"发放"}
+];
+var status_insurance = [
+    {value:0,text:"新增"},
+    {value:1,text:"在保"},
+    {value:2,text:"拟停"},
+    {value:3,text:"停保"}
+];
+var period_product = [
+    {value:0,text:"上班时间"},
+    {value:1,text:"24小时"}
+];
+ var allow_product = [
+     {value:0,text:"第一类"},
+     {value:1,text:"第二类"},
+     {value:2,text:"第三类"},
+     {value:3,text:"第四类"},
+     {value:4,text:"第五类"},
+     {value:5,text:"第六类"}
+ ];
 
 
 //----------------------------------------------------------------字段格式化-----------------------
