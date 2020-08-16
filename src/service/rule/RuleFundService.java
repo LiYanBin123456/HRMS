65e2bdbdd246e21a36694fad171eecbb255bbd23
sxbwelcome <sxbwelcome@163.com>
2020-08-16 10:46:41 +0800

    调整了规则模块的代码结构

--- src/service/rule/RuleFundService.java
+++ src/service/rule/RuleFundService.java
@@ -14,97 +14,69 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
+import java.util.List;
 
-@WebServlet(name="RuleFundServlet",urlPatterns = "/ruleFund")//浏览器发出请求的地址
-public class RuleFundService extends HttpServlet {
-    private static RuleFundDao ruleFundDao = new RuleFundDao();
-    private static QueryConditions queryConditions = new QueryConditions();
-    private static QueryParameter queryParameter = new QueryParameter();
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doPost(request,response);
-    }
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setHeader("Access-Control-Allow-Origin","*");//允许所有域名访问
-        request.setCharacterEncoding("utf-8");
-        response.setContentType("text/html;charset=utf-8");
-        String result;
-        Connection conn = RecruitConnUtil.getConnection();
-
-        String op = request.getParameter("op");
-        switch (op) {
-            case "getList"://获取公积金规则列表
-                result = getList(conn,request);
-                break;
-            case "get"://获取指定公积金规则信息
-                result = get(conn,request);
-                break;
-            case "update"://修改公积金规则信息
-                result = update(conn,request);
-                break;
-            case "insert"://添加公积金规则信息
-                result = insert(conn,request);
-                break;
-            case "delete"://删除公积金规则信息
-                result = delete(conn,request);
-                break;
-            case "getLast"://根据城市模糊查询获取公积金规则信息
-                result = getLast(conn,request);
-                break;
-            default:
-                result = "非法操作";
-        }
-        RecruitConnUtil.closeConnection(conn);
-
-        PrintWriter out = response.getWriter();
-        out.print(result);
-        out.flush();
-        out.close();
-    }
+public class RuleFundService  {
 
     //获取公积金规则列表
-    public String getList(Connection conn, HttpServletRequest request){
-        queryParameter = JSONObject.parseObject(request.getParameter("param"),QueryParameter.class);
-        DaoQueryListResult res = ruleFundDao.getList(conn, queryParameter);
+    public static String getList(Connection conn, HttpServletRequest request){
+        QueryParameter queryParameter = JSONObject.parseObject(request.getParameter("param"),QueryParameter.class);
+        DaoQueryListResult res = RuleFundDao.getList(conn, queryParameter);
         return JSONObject.toJSONString(res);
     }
 
     //获取指定公积金规则信息
-    public String get(Connection conn, HttpServletRequest request) {
+    public static String get(Connection conn, HttpServletRequest request) {
         long id = Long.parseLong(request.getParameter("id"));
+        QueryConditions queryConditions = new QueryConditions();
         queryConditions.add("id", "=", id);
-        DaoQueryResult res = ruleFundDao.get(conn, queryConditions);
+        DaoQueryResult res = RuleFundDao.get(conn, queryConditions);
         return JSONObject.toJSONString(res);
     }
 
     //修改公积金规则信息
-    public String update(Connection conn, HttpServletRequest request) {
-        RuleFund ruleFund = JSON.parseObject(request.getParameter("ruleFund"), RuleFund.class);
-        DaoUpdateResult res = ruleFundDao.update(conn, ruleFund);
+    public static String update(Connection conn, HttpServletRequest request) {
+        RuleFund rule = JSON.parseObject(request.getParameter("rule"), RuleFund.class);
+        DaoUpdateResult res = RuleFundDao.update(conn, rule);
         return JSONObject.toJSONString(res);
     }
 
     //添加公积金规则信息
-    public String insert(Connection conn, HttpServletRequest request) {
-        RuleFund ruleFund = JSON.parseObject(request.getParameter("ruleFund"), RuleFund.class);
-        DaoUpdateResult res = ruleFundDao.insert(conn, ruleFund);
+    public static String insert(Connection conn, HttpServletRequest request) {
+        RuleFund rule = JSON.parseObject(request.getParameter("rule"), RuleFund.class);
+        DaoUpdateResult res = RuleFundDao.insert(conn, rule);
         return JSONObject.toJSONString(res);
     }
 
     //删除公积金规则信息
-    public String delete(Connection conn, HttpServletRequest request) {
+    public static String delete(Connection conn, HttpServletRequest request) {
         long id = Long.parseLong(request.getParameter("id"));
+        QueryConditions queryConditions = new QueryConditions();
         queryConditions.add("id","=",id);
-        DaoUpdateResult res = ruleFundDao.delete(conn, queryConditions);
+        DaoUpdateResult res = RuleFundDao.delete(conn, queryConditions);
         return JSONObject.toJSONString(res);
     }
 
-    //根据城市模糊查询获取公积金规则信息
-    private String getLast(Connection conn, HttpServletRequest request) {
-        queryParameter = JSONObject.parseObject(request.getParameter("param"),QueryParameter.class);
-        queryParameter.addCondition("city","like",queryParameter.conditions.extra);
-        DaoQueryListResult res = ruleFundDao.getList(conn, queryParameter);
-        return JSONObject.toJSONString(res);
+    //获取指定城市最新公积金规则
+    public static String getLast(Connection conn, HttpServletRequest request) {
+        String city = request.getParameter("city");
+        QueryParameter param = new QueryParameter();
+        param.addCondition("city","=",city);
+        param.order.set(true,"start",false);
+        param.pagination.set(true,1,1);
+        DaoQueryListResult res1 = RuleFundDao.getList(conn, param);
+        DaoQueryResult res2 = new DaoQueryResult();
+        if(res1.success) {
+            List<RuleFund> rules = (List<RuleFund>) res1.rows;
+            if (rules.size() > 0) {
+                res2.success = true;
+                res2.data = rules.get(0);
+                return JSONObject.toJSONString(res2);
+            }
+        }
+
+        res2.success = false;
+        res2.msg = "获取公积金规则失败";
+        return JSONObject.toJSONString(res2);
     }
 }
--- src/service/rule/RuleMedicareService.java
+++ src/service/rule/RuleMedicareService.java
@@ -6,105 +6,70 @@ import com.alibaba.fastjson.JSONObject;
 import dao.rule.RuleMedicareDao;
 import database.*;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
+import java.util.List;
 
-@WebServlet(name="RuleMedicareService",urlPatterns = "/ruleMedical")//浏览器发出请求的地址
-public class RuleMedicareService extends HttpServlet {
-    private static RuleMedicareDao ruleMedicareDao = new RuleMedicareDao();
-    private static QueryConditions queryConditions = new QueryConditions();
-    private static QueryParameter queryParameter = new QueryParameter();
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doPost(request,response);
-    }
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setHeader("Access-Control-Allow-Origin","*");//允许所有域名访问
-        request.setCharacterEncoding("utf-8");
-        response.setContentType("text/html;charset=utf-8");
-        String result;
-        Connection conn = RecruitConnUtil.getConnection();
-
-        String op = request.getParameter("op");
-        switch (op) {
-            case "getList"://获取医保规则列表
-                result = getList(conn,request);
-                break;
-            case "get"://获取指定医保规则信息
-                result = get(conn,request);
-                break;
-            case "update"://修改医保规则信息
-                result = update(conn,request);
-                break;
-            case "insert"://添加医保规则信息
-                result = insert(conn,request);
-                break;
-            case "delete"://删除医保规则信息
-                result = delete(conn,request);
-                break;
-            case "getLast"://根据城市模糊查询获取医保规则信息
-                result = getLast(conn,request);
-                break;
-            default:
-                result = "非法操作";
-        }
-        RecruitConnUtil.closeConnection(conn);
-
-        PrintWriter out = response.getWriter();
-        out.print(result);
-        out.flush();
-        out.close();
-    }
-
+public class RuleMedicareService {
     //获取医保规则列表
-    public String getList(Connection conn, HttpServletRequest request){
-        queryParameter = JSONObject.parseObject(request.getParameter("param"),QueryParameter.class);
-        DaoQueryListResult res = ruleMedicareDao.getList(conn, queryParameter);
+    public static String getList(Connection conn, HttpServletRequest request){
+        QueryParameter queryParameter = JSONObject.parseObject(request.getParameter("param"),QueryParameter.class);
+        DaoQueryListResult res = RuleMedicareDao.getList(conn, queryParameter);
         return JSONObject.toJSONString(res);
     }
 
     //获取指定医保规则信息
-    public String get(Connection conn, HttpServletRequest request) {
+    public static String get(Connection conn, HttpServletRequest request) {
         long id = Long.parseLong(request.getParameter("id"));
-        queryConditions.add("id", "=", id);
-        DaoQueryResult res = ruleMedicareDao.get(conn, queryConditions);
+        QueryConditions conditions = new QueryConditions();
+        conditions.add("id", "=", id);
+        DaoQueryResult res = RuleMedicareDao.get(conn, conditions);
         return JSONObject.toJSONString(res);
     }
 
     //修改医保规则信息
-    public String update(Connection conn, HttpServletRequest request) {
-        RuleMedicare ruleMedicare = JSON.parseObject(request.getParameter("ruleMedicare"), RuleMedicare.class);
-        DaoUpdateResult res = ruleMedicareDao.update(conn, ruleMedicare);
+    public static String update(Connection conn, HttpServletRequest request) {
+        RuleMedicare rule = JSON.parseObject(request.getParameter("rule"), RuleMedicare.class);
+        DaoUpdateResult res = RuleMedicareDao.update(conn, rule);
         return JSONObject.toJSONString(res);
     }
 
     //添加医保规则信息
-    public String insert(Connection conn, HttpServletRequest request) {
-        RuleMedicare ruleMedicare = JSON.parseObject(request.getParameter("ruleMedicare"), RuleMedicare.class);
-        DaoUpdateResult res = ruleMedicareDao.insert(conn, ruleMedicare);
+    public static String insert(Connection conn, HttpServletRequest request) {
+        RuleMedicare rule = JSON.parseObject(request.getParameter("rule"), RuleMedicare.class);
+        DaoUpdateResult res = RuleMedicareDao.insert(conn, rule);
         return JSONObject.toJSONString(res);
     }
 
     //删除医保规则信息
-    public String delete(Connection conn, HttpServletRequest request) {
+    public static String delete(Connection conn, HttpServletRequest request) {
         long id = Long.parseLong(request.getParameter("id"));
-        queryConditions.add("id","=",id);
-        DaoUpdateResult res = ruleMedicareDao.delete(conn, queryConditions);
+        QueryConditions conditions = new QueryConditions();
+        conditions.add("id","=",id);
+        DaoUpdateResult res = RuleMedicareDao.delete(conn, conditions);
         return JSONObject.toJSONString(res);
     }
 
     //根据城市模糊查询获取医保规则信息
-    private String getLast(Connection conn, HttpServletRequest request) {
-        queryParameter = JSONObject.parseObject(request.getParameter("param"),QueryParameter.class);
-        queryParameter.addCondition("city","like",queryParameter.conditions.extra);
-        DaoQueryListResult res = ruleMedicareDao.getList(conn, queryParameter);
-        return JSONObject.toJSONString(res);
+    public static String getLast(Connection conn, HttpServletRequest request) {
+        String city = request.getParameter("city");
+        QueryParameter param = new QueryParameter();
+        param.addCondition("city","=",city);
+        param.order.set(true,"start",false);
+        param.pagination.set(true,1,1);
+        DaoQueryListResult res1 = RuleMedicareDao.getList(conn, param);
+        DaoQueryResult res2 = new DaoQueryResult();
+        if(res1.success) {
+            List<RuleMedicare> rules = (List<RuleMedicare>) res1.rows;
+            if (rules.size() > 0) {
+                res2.success = true;
+                res2.data = rules.get(0);
+                return JSONObject.toJSONString(res2);
+            }
+        }
+
+        res2.success = false;
+        res2.msg = "获取医保规则失败";
+        return JSONObject.toJSONString(res2);
     }
 }
